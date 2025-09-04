a630f77d0c472fe2e88c487b068a81224c4e1cd1
kami-fumei <imrankh7785@outlook.com>
2025-09-04 20:34:23 +0530

    Add project files.

---
+++ SimWheeel_Receiver/Lib_Simwheel.h
@@ -0,0 +1,8 @@
+#pragma once
+#ifdef Lib_Simwheel
+#define Lib_Simwheel
+
+void ShowLocalIP();
+
+#endif // Lib_Simwheel
+
---
+++ SimWheeel_Receiver/wifi.cpp
@@ -0,0 +1,47 @@
+//#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <iostream>
+#include <iphlpapi.h>
+
+#pragma comment(lib, "iphlpapi.lib")
+//#pragma comment(lib, "Ws2_32.lib")
+
+
+void ShowLocalIP() {
+    ULONG flags = GAA_FLAG_INCLUDE_PREFIX;
+    ULONG family = AF_INET; // Only IPv4. Use AF_UNSPEC for both IPv4 & IPv6
+
+    ULONG bufLen = 15000;
+    PIP_ADAPTER_ADDRESSES adapters = (IP_ADAPTER_ADDRESSES*)malloc(bufLen);
+
+    if (adapters == NULL) { // Check for memory allocation failure
+        printf("Memory allocation failed\n");
+        return;
+    }
+
+    DWORD ret = GetAdaptersAddresses(family, flags, NULL, adapters, &bufLen);
+    if (ret != NO_ERROR) {
+        printf("GetAdaptersAddresses failed with error: %lu\n", ret);
+        free(adapters);
+        return;
+    }
+    printf("Use this/those ip in app to connect\n");
+
+    for (PIP_ADAPTER_ADDRESSES aa = adapters; aa != NULL; aa = aa->Next) {
+        // Skip down/loopback interfaces
+        if (aa->OperStatus != IfOperStatusUp || aa->IfType == IF_TYPE_SOFTWARE_LOOPBACK)
+            continue;
+
+        for (PIP_ADAPTER_UNICAST_ADDRESS ua = aa->FirstUnicastAddress; ua != NULL; ua = ua->Next) {
+            SOCKADDR* addr = ua->Address.lpSockaddr;
+            if (addr->sa_family == AF_INET) {
+                char ip[INET_ADDRSTRLEN];
+                sockaddr_in* ipv4 = (sockaddr_in*)addr;
+                inet_ntop(AF_INET, &ipv4->sin_addr, ip, sizeof(ip));
+                printf("Local IPv4: %s \n", ip);
+            }
+        }
+    }
+
+    free(adapters);
+}
