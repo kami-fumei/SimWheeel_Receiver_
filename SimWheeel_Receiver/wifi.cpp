d508ff04599b14fedfbc5bd7d24f8ba602187839
kami-fumei <imrankh7785@outlook.com>
2025-09-05 14:31:11 +0530

    new

--- SimWheeel_Receiver/wifi.cpp
+++ SimWheeel_Receiver/wifi.cpp
@@ -1,11 +1,4 @@
-//#include <winsock2.h>
-#include <ws2tcpip.h>
-#include <iostream>
-#include <iphlpapi.h>
-
-#pragma comment(lib, "iphlpapi.lib")
-//#pragma comment(lib, "Ws2_32.lib")
-
+#include "Lib_Simwheel.h"
 
 void ShowLocalIP() {
     ULONG flags = GAA_FLAG_INCLUDE_PREFIX;
